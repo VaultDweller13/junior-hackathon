e1e5af17b5ad6d694be07b965785865a4999fe4e
chewho <molocrips@gmail.com>
2023-11-11 11:47:36 +0100

    refactor: add counter and timer

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -4,6 +4,10 @@ const MainLayout = () => {
   return (
     <>
       <h2 className={styles.header}>Memory Game</h2>
+      <div className={styles.info}>
+        <p>Time: 100</p>
+        <p>Flips: 0</p>
+      </div>
       <div className={styles.field}></div>
       <button className={styles.button}>Reset Game</button>
     </>
