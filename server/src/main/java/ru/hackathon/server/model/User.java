6dff5497651ae32ce3794eecbed711a96a3e7a2e
EVGENII PELIPETS <eugenepelipets@gmail.com>
2023-11-11 14:46:44 +0300

    feat: add User model and User DB for test, spring initializr

---
+++ server/src/main/java/ru/hackathon/server/model/User.java
@@ -0,0 +1,26 @@
+package ru.hackathon.server.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "score")
+    private Long score;
+
+}
